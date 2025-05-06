165495463cb683e6c42310b9747ad07525f3632b
MatiasWilder <48178322@est.ort.edu.ar>
2025-05-06 09:04:56 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import formulario from './form.jsx';
+import listado from './listado.jsx';
+
+
+function App() {
+
+
+  return (
+    
+
+  )
+}
+
+export default App
---
+++ src/listado.jsx
@@ -0,0 +1,10 @@
+import cita from './cita.jsx';
+
+
+return (
+    <h2>Listado</h2>
+    </cita>
+    </cita>
+    </cita>
+
+);
