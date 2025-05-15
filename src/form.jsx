87eb51b900c231eb6a367224a53ceaa103dc7051
Android Studio <devandroid@ort>
2025-05-15 09:03:37 -0300

    Formulario base (Clase explicacion UseEffect)

--- src/form.jsx
+++ src/form.jsx
@@ -2,6 +2,16 @@ function Formulario() {
     return (
       <div className="formulario">
         <h2>Formulario</h2>
+        <label>Nombre Mascota</label>
+        <input></input>
+        <br></br><label>Nombre Dueño</label>
+        <input></input>
+        <br></br><label>Fecha</label>
+        <input></input>
+        <br></br><label>Hora</label>
+        <input></input>
+        <br></br><label>Síntoma</label>
+        <input></input>
       </div>
     )
   }
